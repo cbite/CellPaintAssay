ababbfaec6d0358d8324d4e2e84ef3f40e122749
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-05-17 11:25:42 +0200

    added missing value check

--- 01_NormalizationAcrossPlates/readData.py
+++ 01_NormalizationAcrossPlates/readData.py
@@ -3,6 +3,15 @@
 import pandas as pd
 
 def readData(input_file=None,seperator=None,set_headers='infer'):
-    data_file=pd.read_csv(input_file,sep=seperator,headers=set_headers)
+    data_file=pd.read_csv(input_file,sep=seperator,header=set_headers)
     print('Number of rows is %d and number of columns is %d ' % (data_file.shape))
     return data_file
+
+def checkMissingValues(input_data_frame):
+    missing_values=input_data_frame.isnull()
+    any_missing_values=any(missing_values)
+    print('Missing values check returned: %s' % any_missing_values)
+    missing_value_in_column_name=input_data_frame.columns[any_missing_values]
+    print('There are %d columns with missing values' % missing_value_in_column_name[0].__len__())
+    print('The following columns have missing values %s' % missing_value_in_column_name)
+    return missing_values
